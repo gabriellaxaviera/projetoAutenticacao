265aa819a7eefccd68e8b778738a86afdf308249
Arruda <gabriella.x.arruda@accenture.com>
2021-02-10 15:50:30 -0300

    iniciando configuracoes do projeto de autenticacao via API

---
+++ src/main/java/com/piloto/autenticacao/ProjetoAutenticacaoApplication.java
@@ -0,0 +1,13 @@
+package com.piloto.autenticacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAutenticacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAutenticacaoApplication.class, args);
+	}
+
+}
