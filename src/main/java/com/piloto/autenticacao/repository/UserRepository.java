ccbcd17cb5aa60a7c62af65405195eb3cf71cd0d
Amanda Veiga <49622800+mandaveiga@users.noreply.github.com>
2021-03-15 12:41:23 -0300

    Revert "Revert "Feature/database""

---
+++ src/main/java/com/piloto/autenticacao/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.piloto.autenticacao.repository;
+
+import com.piloto.autenticacao.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    User findByCpf(String cpf);
+
+}
